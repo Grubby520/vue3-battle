a083602fe5d3fa6f605859b3b0b9752b86b85b65
wanglei <wlei-wang@starlinke.com>
2020-11-13 11:22:46 +0800

    wlei:框架搭建-登录UI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,22 @@
 <template>
 <div class="sl-login-container">
-    <el-form class="sl-login" :model="loginForm" :rules="loginRules" ref="loginForm"
-        label-position="left">
-        <div class="title-container">
-          <h3 class="title">{{systemName}}</h3>
-        </div>
+  <div class="sl-login">
+    <h3 class="sl-login-title">{{systemName}}</h3>
+    <el-form ref="loginForm" class="sl-login-form" :model="loginForm" :rules="loginRules" autocomplete="off">
         <el-form-item prop="username">
-            <span class="el-icon-s-custom"></span>
+            <span class="el-icon-s-custom form-ite-icon"></span>
             <el-input name="username" type="text" v-model="loginForm.username"
               placeholder="username" />
         </el-form-item>
         <el-form-item prop="password">
-            <span class="el-icon-lock"></span>
+            <span class="el-icon-lock form-ite-icon"></span>
             <el-input name="password" type="password" v-model="loginForm.password"
                 placeholder="password" />
         </el-form-item>
-        <el-button ref="loginBtn" type="primary">{{registerText}}</el-button>
-        <el-button ref="loginBtn" type="primary" @click="login">{{loginText}}</el-button>
+        <el-button type="primary" class="mr-2rem">{{registerText}}</el-button>
+        <el-button type="primary" @click="login">{{loginText}}</el-button>
     </el-form>
+  </div>
 </div>
 </template>
 
@@ -25,7 +24,7 @@
 export default {
   data () {
     return {
-      systemName: 'Starlink  供 应 商 平 台',
+      systemName: 'Starlink供应商平台',
       registerText: '注册',
       loginText: '登录',
       loginForm: {
@@ -70,7 +69,6 @@ export default {
 
 <style scoped lang="scss">
 @import '@assets/scss/_var.scss';
-
 .sl-login-container {
   height: 100%;
   background-color: $color-login-bg;
@@ -79,13 +77,68 @@ export default {
   background-repeat: no-repeat;
 }
 
+.sl-login-container /deep/ {
+  .el-input__inner {
+    display: inline-block;
+    height: 3rem;
+    line-height: 3rem;
+    padding: 0 0.3em 0 2em;
+  }
+}
+
+.sl-login-title {
+  margin-bottom: 1em;
+  font-size: 1.6rem;
+  color: $color-white;
+  letter-spacing: 0.2em;
+}
+
 .sl-login {
   display: inline-block;
   position: absolute;
+  width: 25%;
   left: 50%;
-  top: 50%;
+  top: 40%;
   padding: 1em;
   transform: translate(-50%,-50%);
+}
+
+.form-ite-icon {
+  position: absolute;
+  left: 0.5em;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 10;
+}
+
+@media only screen and (max-width: 1920px) {
+  .sl-login{
+    width: 25%;
+  }
+}
+
+@media only screen and (max-width: 1366px) {
+  .sl-login{
+    width: 30%;
+  }
+}
+
+@media only screen and (max-width: 960px) {
+  .sl-login{
+    width: 40%;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .sl-login{
+    width: 60%;
+  }
+}
+
+@media only screen and (max-width: 300px) {
+  .sl-login{
+    width: 98%;
+  }
 }
 
 </style>
