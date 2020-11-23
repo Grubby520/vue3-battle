f29809764185ec46f1a46410f31b36ae88fc1dae
王艺锦 <wangyijin@starlinke.com>
2020-11-23 17:09:34 +0800

    增加jsconfig.js文件

--- src/api/recommendProducts/recommendProductsUrl.js
+++ src/api/recommendProducts/recommendProductsUrl.js
@@ -2,7 +2,7 @@ const RECOMMENDURLS = {
   // odm 列表页
   RECOMMENDPRODUCTS: '/product/list',
   // 上传SPU数据 post
-  UPLOAD_SPU_DATA: 'http://10.250.1.205:9300/product/importSpu',
+  UPLOAD_SPU_DATA: '/product/importSpu',
   // odm推品
   RECOMMEND: '/product/pushed',
   // odm 取消推品
