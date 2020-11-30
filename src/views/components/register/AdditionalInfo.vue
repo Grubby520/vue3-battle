bdc97ea3cd72657d0f345435feb3a535f679b854
wanglei <wlei-wang@starlinke.com>
2020-11-30 15:03:19 +0800

    wlei:task #3018 注册-提交入驻申请

---
+++ src/views/components/register/AdditionalInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'AdditionalInfo',
+  props: {
+  },
+  data: () => {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+  },
+  mounted: function () {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@assets/scss/_var.scss';
+</style>
