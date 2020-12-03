3299109005db49d7843d532b37a0de4ba8582465
liuxiaoxue <liuxiaoxue@starlinke.com>
2020-12-03 15:11:10 +0800

    接口调整

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 // 用于声明调用接口的方法
 import URL from './url'
-import { get } from '@shared/http'
+import { get, post } from '@shared/http'
 import { yesOrNo } from './static'
 
 const COMMONAPI = {
@@ -20,7 +20,7 @@ const COMMONAPI = {
   },
   // 预上传oss接口
   getOssUrl (params) {
-    return get(URL.ossUrl, { ...params })
+    return post(URL.ossUrl, params)
   },
   // 预删除oss地址
   deleteOssUrl (params) {
--- src/api/url.js
+++ src/api/url.js
@@ -1,11 +1,12 @@
 // 用于存放接口地址
 const baseURL = '/srm-goods-service'
+const productUrl = '/srm-common-service'
 const URL = {
   dictUrl: 'srm-common-service/data/name-value/list',
   // 分类树
-  categoryUrl: baseURL + '/products/category/tree',
+  categoryUrl: productUrl + '/product-service/category/tree',
   // oss预上传地址
-  ossUrl: baseURL + '/productImage/generatePreUploadUrl',
+  ossUrl: '/srm-common-service' + '/aliyun-oss/generatePreUploadUrl',
   // oss 预删除地址
   deleteOssUrl: baseURL + '/productImage/generatePreDeleteUrl'
 }
