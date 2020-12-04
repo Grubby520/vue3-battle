38aef6e95b972ec636a2706cebaaaf1e8f0ff638
yxm <731994937@qq.com>
2020-12-04 20:07:02 +0800

    删除调试菜单

--- src/router/homeRoutes.js
+++ src/router/homeRoutes.js
@@ -77,14 +77,5 @@ export const homeRoutes = [
         component: () => import('@/views/ImportProductImgs')
       }
     ]
-  },
-  {
-    path: 'createProduct',
-    name: '创建产品',
-    meta: {
-      icon: 'el-icon-postcard',
-      code: 'menu_my_info'
-    },
-    component: () => import('@/views/createProduct/index.vue')
   }
 ]
