5ccf8bf018a01e4b605f1fa90ee05d92582bf284
liuxiaoxue <liuxiaoxue@starlinke.com>
2020-12-04 13:43:04 +0800

    图片组件限制上传数量

--- src/shared/components/base/SlUploadImages.vue
+++ src/shared/components/base/SlUploadImages.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="uploadImage">
-    <!-- {{images}} -->
     <!-- 上传图片 -->
     <el-upload
-      action="http://srm-storage-test.oss-cn-shanghai.aliyuncs.com"
+      action="#"
       ref="uploader"
       :disabled="disabled"
       list-type="picture-card"
-      :limit="200"
-      accept="image/png, image/jpeg, image/jpg, image/bmp"
+      :accept="accept"
       :file-list="fileList"
+      :limit="limit"
       :http-request="uploadFile"
       :on-exceed="handleExceed"
-      :on-change="onChange"
       :before-upload="beforeUpload"
     >
       <i slot="default" class="el-icon-plus"></i>
@@ -59,12 +57,44 @@ export default {
     event: 'changeUploadImages'
   },
   props: {
-    images: { type: Array, required: false, default: () => { return [] } },
+    images: {
+      type: Array,
+      required: false,
+      default: () => { return [] }
+    },
     // 需要回显图片数组
-    imageUrls: { type: Array, required: false, default: () => { return [] } },
+    imageUrls: {
+      type: Array,
+      required: false,
+      default: () => { return [] }
+    },
     // 0为商品图片 1为尺寸图片
-    imageType: { type: Number, required: false, default: undefined },
-    disabled: { type: Boolean, required: false, default: false }
+    imageType: {
+      type: Number,
+      required: false,
+      default: undefined
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    // 当图片类型为产品图片、尺码图片时需传入产品spu编码，当图片类型为资质图片需传入供应商营业执照编号
+    folder: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    limit: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    accept: {
+      type: String,
+      required: false,
+      default: 'image/png, image/jpeg, image/jpg, image/bmp'
+    }
   },
   data () {
     return {
@@ -85,28 +115,25 @@ export default {
       immediate: true
     }
   },
-  mounted () {
-    this.$emit('imageUrls')
-  },
   methods: {
-    async beforeUpload (file) {
-      // 上传支持的大小
-      const LIMITM = file.size / 1024 / 1024 <= 4
-      // 上传支持的格式
-      const TYPE = ['image/jpeg', 'image/jpg', 'image/png', 'image/bmp']
-      const LIMITIMAGE = TYPE.includes(file.type)
-      // 图片宽高比
-      const LIMITSIZE = await this.limitSizes(file)
-      if (!LIMITM) {
-        this.$message.error('上传图片大小不能超过 4MB!')
-        this.cancelUpload(file)
-      } else if (!LIMITIMAGE) {
-        this.$message.error('上传图片格式不正确!')
-        this.cancelUpload(file)
-      } else if (!LIMITSIZE) {
-        this.$message.error('请上传1:1或者4:3,宽高值最大尺寸为4096px-4096px的图片!')
-        this.cancelUpload(file)
-      }
+    beforeUpload (file) {
+      return new Promise(async (resolve, reject) => {
+        // 上传支持的大小
+        const LIMITM = file.size / 1024 / 1024 <= 4
+        // 图片宽高比
+        const LIMITSIZE = await this.limitSizes(file)
+        if (!LIMITM) {
+          reject(new Error(this.$message.error('上传图片大小不能超过 4MB!')))
+        } else if (this.accept.indexOf(file.type) === -1) {
+          reject(new Error(this.$message.error('上传图片格式不正确!')))
+        } else if (!LIMITSIZE) {
+          reject(new Error(this.$message.error('请上传1:1或者4:3,宽高值最大尺寸为4096px-4096px的图片!')))
+        } else if (this.fileList.some(item => item.name === file.name)) {
+          reject(new Error(this.$message.error('不能上传相同的图片!')))
+        } else {
+          resolve(true)
+        }
+      })
     },
 
     limitSizes (file) {
@@ -136,31 +163,19 @@ export default {
     },
 
     handleExceed () {
-      this.$message.error(`上传图片不能超过20张!`)
-    },
-    onChange (file, fileList) {
-      // 上传的图片列表中包含本次上传的图片就放弃上传
-      if (this.fileList.some(item => item.name === file.name)) {
-        // 取消上传
-        this.$refs.uploader.abort(file)
-        // 删除钩子列表中数据
-        fileList.splice(fileList.indexOf(file), 1)
-        this.$message.error('不能上传相同的图片!')
-      } else {
-        this.fileList.push(file)
-      }
+      this.$message.error(`上传图片不能超过${this.limit}张!`)
     },
 
     handleRemove (file) {
-      console.log('remove', file)
       this.$confirm('确实要删除该图片吗？', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.cancelUpload(file)
-        const filImages = this.images.filter(img => img.name !== file.name)
-        this.$emit('changeUploadImages', filImages)
+        // 取消上传文件
+        this.$refs.uploader.abort(file)
+        // 上传列表中删除取消文件
+        this.fileList.splice(this.fileList.indexOf(file), 1)
       })
     },
 
@@ -173,7 +188,7 @@ export default {
       downloadFile(file.url, file.name)
     },
     uploadFile (file) {
-      const PARAMS = { 'folder': 'dddd', 'fileName': file.file.name, 'contentType': file.file.type, 'fileType': this.imageType }
+      const PARAMS = { 'folder': this.folder, 'fileName': file.file.name, 'contentType': file.file.type, 'fileType': this.imageType }
 
       // 获取预上传oss地址
       CommonApi.generatePreUploadUrl(PARAMS)
@@ -181,12 +196,14 @@ export default {
           console.log(res.data)
           res.data.file = file.file
           fileToMd5(file.file).then((md5) => {
+            res.data.uid = file.file.uid
             res.data.src = res.data.showUrl
             res.data.url = res.data.showUrl
             res.data.hash = md5
             res.data.name = file.file.name
             res.data.imageType = this.imageType
             this.ossImages.push(res.data)
+            console.log(res.data)
             this.gotoOss(res.data.preUploadUrl, file.file)
           })
         })
@@ -199,15 +216,7 @@ export default {
           this.$emit('changeUploadImages', IMAGES)
           IMAGES.push(...this.ossImages)
         })
-    },
-
-    cancelUpload (file) {
-      // 取消上传文件
-      this.$refs.uploader.abort(file)
-      // 上传列表中删除取消文件
-      this.fileList.splice(this.fileList.indexOf(file), 1)
     }
-
   }
 
 }
