d50cf0fe8163ea054e174f9278dd90280e0c2cb2
wanglei <wlei-wang@starlinke.com>
2020-12-04 13:30:53 +0800

    wlei：注册接口添加更新接口

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -5,6 +5,7 @@ const USER_API = {
   authLogin: (params) => post(URL.authLogin, params),
   modifyPassword: (params) => post(URL.modifyPassword, params),
   register: (params) => post(URL.register, params),
+  registerUpdate: (params) => post(URL.registerUpdate, params),
   getUserInfo: () => get(URL.userInfo),
   logout: () => get(URL.logout, {}, { addLoading: true }),
   resetPassword: (params) => get(URL.resetPassword, params, { addLoading: true }),
