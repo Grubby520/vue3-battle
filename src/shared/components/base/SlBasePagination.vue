933cb03c92b2f989be60d652308f49c61d5099f3
wanglei <wlei-wang@starlinke.com>
2021-01-29 15:40:15 +0800

    wlei:限制表格每页数量最大值

--- src/shared/components/base/SlBasePagination.vue
+++ src/shared/components/base/SlBasePagination.vue
@@ -30,7 +30,7 @@ export default {
   },
   data () {
     return {
-      pageSizes: [10, 20, 50, 100, 200],
+      pageSizes: [10, 20, 50],
       page: {
         pageIndex: 1,
         pageSize: 10
