5fbd528d8077ae47df4f38451025660f991ee714
wanglei <wlei-wang@starlinke.com>
2021-03-17 10:31:03 +0800

    wlei：删掉没使用的代码

--- src/router/homeRoutes.js
+++ src/router/homeRoutes.js
@@ -83,24 +83,6 @@ export const homeRoutes = [
           }
         },
         component: () => import('@/views/components/recommendProducts/OdmDetail.vue')
-      },
-      {
-        path: 'import-spu',
-        name: '导入SPU',
-        meta: {
-          icon: '',
-          notMenu: true
-        },
-        component: () => import('@/views/ImportSpu.vue')
-      },
-      {
-        path: 'import-product-imgs',
-        name: '导入商品图片',
-        meta: {
-          icon: '',
-          notMenu: true
-        },
-        component: () => import('@/views/ImportProductImgs')
       }
     ]
   },
