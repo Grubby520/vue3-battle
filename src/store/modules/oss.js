e19bb92c1780f9b04e87d87b269f84a29eba01e4
wanglei <wlei-wang@starlinke.com>
2021-03-23 11:05:21 +0800

    wlei:修改正则

--- src/store/modules/oss.js
+++ src/store/modules/oss.js
@@ -20,7 +20,7 @@ export default {
     UPLOAD_FILE ({ commit, dispatch }, params) {
       let { preUploadUrl, file } = params
       // 解决非相同协议上传oss失败的问题
-      let uploadUrl = preUploadUrl.replace(/^(http|https)?=(:\/\/)/, window.location.protocol.split(':')[0])
+      let uploadUrl = preUploadUrl.replace(/^(http|https)/, window.location.protocol.split(':')[0])
       return put(uploadUrl, file, {
         headers: { 'Content-Type': file.type ? file.type : 'application/octet-stream' },
         cancelToken: new CancelToken(function executor (c) {
