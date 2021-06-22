30e82afa6ed3a7a71807e77a73f68525425660e7
zhangjian1 <zhangjian1@starlinke.com>
2021-06-22 17:54:23 +0800

    修改样式

--- src/views/deliveryManage/LogisticsInfoDialog.vue
+++ src/views/deliveryManage/LogisticsInfoDialog.vue
@@ -1,37 +1,41 @@
 <template>
-  <el-dialog
-    title="物流信息"
-    :visible.sync="isShowLogistics"
-    width="40%"
-    center
-    class="logistics-dialog"
-  >
-    <div class="content">
-      <div style="margin-bottom:10px;"><el-link type="primary" @click="print" v-if="orderStatus<5">物流条码打印</el-link></div>
-      <div class="bar-code-wrap">
-        <div class="print" id="printMe_logisticsNumber">
-        <div class="tit">物流单号</div>
-        <div><svg class="barcodeSvg"/></div>
-        <div class="logisticsNumber">{{logisticsInfo.logisticsNumber}}</div>
-      </div>
-      </div>
-      <h3>物流信息</h3>
-      <div class="logistics-info">
-        <div class="line-hight32">
-          <p>物流单号：{{logisticsInfo.logisticsNumber}}（{{logisticsInfo.logisticsCompanyName}}）</p>
-          <p>发货时间：{{logisticsInfo.deliveryTime}}</p>
+    <el-dialog
+        title="物流信息"
+        :visible.sync="isShowLogistics"
+        width="40%"
+        center
+        class="logistics-dialog"
+    >
+        <div class="content">
+            <div style="margin-bottom:10px;">
+                <el-link type="primary" @click="print" v-if="orderStatus<5">物流条码打印</el-link>
+            </div>
+            <div class="bar-code-wrap">
+                <div class="print" id="printMe_logisticsNumber">
+                    <div class="tit">物流单号</div>
+                    <div>
+                        <svg class="barcodeSvg" />
+                    </div>
+                    <div class="logisticsNumber">{{logisticsInfo.logisticsNumber}}</div>
+                </div>
+            </div>
+            <h3>物流信息</h3>
+            <div class="logistics-info">
+                <div class="line-hight32">
+                    <p>物流单号：{{logisticsInfo.logisticsNumber}}（{{logisticsInfo.logisticsCompanyName}}）</p>
+                    <p>发货时间：{{logisticsInfo.deliveryTime}}</p>
+                </div>
+                <div class="line-hight32">
+                    <p>发货单号：{{logisticsInfo.orderNumber}}</p>
+                    <p>签收时间：{{logisticsInfo.submissionTime}}</p>
+                </div>
+            </div>
+            <el-table :data="tableData" style="width: 100%" border max-height="240px">
+                <el-table-column prop="date" label="时间" width="140px" align="center"></el-table-column>
+                <el-table-column prop="content" label="物流信息" align="center"></el-table-column>
+            </el-table>
         </div>
-        <div class="line-hight32">
-          <p>发货单号：{{logisticsInfo.orderNumber}}</p>
-          <p>签收时间：{{logisticsInfo.submissionTime}}</p>
-        </div>
-      </div>
-      <el-table :data="tableData" style="width: 100%" border max-height="240px">
-        <el-table-column prop="date" label="时间" width="140px" align="center"></el-table-column>
-        <el-table-column prop="content" label="物流信息" align="center"></el-table-column>
-      </el-table>
-    </div>
-  </el-dialog>
+    </el-dialog>
 </template>
 <script>
 import JsBarcode from 'jsbarcode'
@@ -81,39 +85,39 @@ export default {
 </script>
 <style lang="scss" scoped>
 .logistics-dialog {
-  /deep/.el-dialog {
-    height: 420px;
-    /deep/ .el-dialog__body {
-      padding: 0px 25px 30px;
+    /deep/.el-dialog {
+        height: 460px;
+        /deep/ .el-dialog__body {
+            padding: 0px 25px 30px;
+        }
     }
-  }
 }
 .logistics-info {
-  margin: 16px 0;
-  display: flex;
-  justify-content: space-between;
-  .line-hight32 {
-    line-height: 20px;
-  }
+    margin: 16px 0;
+    display: flex;
+    justify-content: space-between;
+    .line-hight32 {
+        line-height: 20px;
+    }
 }
 .bar-code-wrap {
     overflow: hidden;
     clip-path: polygon(0px 0px, 0px 0px, 0px 0px, 0px 0px);
     display: none;
 }
-.print{
-  width: 58mm;
-  height: 38mm;
-  padding: 1mm;
-  text-align: center;
-  .tit{
-    font-size: 18px;
-    margin-bottom: 5px;
-    margin-top: 10px;
-  }
-  .logisticsNumber{
-    font-size: 14px;
-    line-height: 30px;
-  }
+.print {
+    width: 58mm;
+    height: 38mm;
+    padding: 1mm;
+    text-align: center;
+    .tit {
+        font-size: 18px;
+        margin-bottom: 5px;
+        margin-top: 10px;
+    }
+    .logisticsNumber {
+        font-size: 14px;
+        line-height: 30px;
+    }
 }
 </style>
